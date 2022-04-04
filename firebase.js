be1a9f0607638c8b7c8954cf76f2c4d69e20c77e
Kelton Day <keltonday@yahoo.com>
2022-04-04 14:43:53 -0500

    added auth and add to basket

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABoUHhsEdWJQxHd8ZvVM4EBLGx5VnbBac",
+    authDomain: "react-c7d6f.firebaseapp.com",
+    projectId: "react-c7d6f",
+    storageBucket: "react-c7d6f.appspot.com",
+    messagingSenderId: "994861232477",
+    appId: "1:994861232477:web:1bdcd6bc82251cc338b8b4"
+    };
